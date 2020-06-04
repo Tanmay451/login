ecc30b12ff936d5f457e36d878573c4518bf307d
Tanmay451 <tanmaypiyushkaushik451@gmail.com>
2020-06-04 10:33:02 +0530

    added modules for user check

--- models/models.go
+++ models/models.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"database/sql"
 	"log"
 	"login/config"
 )
@@ -9,45 +8,70 @@ import (
 //User user
 type User struct {
 	ID       int64  `json:"id"`
-	Name     string `json:"name"`
+	Name     string `json:"username"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
 //CreateUser get new job id for old sid
-func CreateUser(user User) int {
+func CreateUser(user User) (int, error) {
 	db := config.GetDB()
 
-	rows := db.QueryRow("INSERT INTO db_user(name,email,password) "+user.Name, user.Email, user.Password)
+	rows := db.QueryRow("INSERT INTO account(username,email,password) RETURNING user_id"+user.Name, user.Email, user.Password)
 
-	var nid int
-	err := rows.Scan(&nid)
+	var userID int
+	err := rows.Scan(&userID)
 
 	if err != nil {
 		log.Println(err)
-		if err == sql.ErrNoRows {
-			// no such job id
-			return 0
-		}
 	}
-	return nid
+	return userID, err
 }
 
 //GetUser get new job id for old sid
-func GetUser(user User) int {
+func GetUser(user User) (bool, User) {
 	db := config.GetDB()
 
-	rows := db.QueryRow("SELECT id, password from db_user where email = " + user.Email)
+	rows := db.QueryRow("SELECT user_id, name, password from account where email = " + user.Email)
 
-	var nid int
-	err := rows.Scan(&nid)
+	var userID int64
+	var name string
+	var password string
+
+	err := rows.Scan(&userID, &name, &password)
 
 	if err != nil {
 		log.Println(err)
-		if err == sql.ErrNoRows {
-			// no such job id
-			return 0
-		}
 	}
-	return nid
+	var flag bool
+	if password == user.Password {
+		flag = true
+		user.ID = userID
+		user.Name = name
+	}
+	// flag = true correct password
+	// flag = false wrong password
+	return flag, user
+}
+
+// CheckUser check for user email in database
+func CheckUser(user User) bool {
+	db := config.GetDB()
+
+	rows := db.QueryRow("SELECT count(*) from account where email = " + user.Email)
+
+	var count int
+
+	err := rows.Scan(&count)
+
+	if err != nil {
+		log.Println(err)
+	}
+	var flag bool
+	if count > 0 {
+		flag = true
+	}
+	// flag = true user exist
+	// flag = flase user not exist
+	return flag
 }
